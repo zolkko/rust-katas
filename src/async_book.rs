1acd24bf1eb5da984f95ada80e2f35c8c4d19156
zolkko <zolkko@gmail.com>
2019-09-10 12:28:57 +0300

    chore: add timer future

--- src/async_book.rs
+++ src/async_book.rs
@@ -1,4 +1,12 @@
+use std::future::Future;
+use std::pin::Pin;
+use std::sync::{Arc, Mutex};
+use std::task::{Context, Poll, Waker};
+use std::thread;
+use std::time::Duration;
+
 use futures::executor::block_on;
+
 
 async fn hello() {
     print!("hello");
@@ -43,10 +51,58 @@ async fn async_main() {
     futures::join!(f1, f2);
 }
 
+struct SharedState {
+    completed: bool,
+    waker: Option<Waker>,
+}
+
+pub struct TimerFuture {
+    shared_state: Arc<Mutex<SharedState>>,
+}
+
+impl Future for TimerFuture {
+    type Output = ();
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let mut shared_state = self.shared_state.lock().unwrap();
+        if shared_state.completed {
+            Poll::Ready(())
+        } else {
+            shared_state.waker = Some(cx.waker().clone());
+            Poll::Pending
+        }
+    }
+}
+
+fn timer(duration: Duration) -> TimerFuture {
+    let mut state = Arc::new(Mutex::new(SharedState { completed: false, waker: None }));
+    let mut thread_state = Arc::clone(&state);
+
+    thread::spawn(move || {
+        thread::sleep(duration);
+
+        let mut ss = thread_state.lock().unwrap();
+        ss.completed = true;
+        if let Some(waker) = ss.waker.take() {
+            waker.wake();
+        }
+    });
+
+    TimerFuture { shared_state: state, }
+}
+
+async fn timer_test() {
+    println!("before timer");
+    timer(Duration::from_secs(5)).await;
+    println!("after timer");
+}
+
 fn main() {
     let future = hello_world();
     block_on(future);
 
     let future = async_main();
+    block_on(future);
+
+    let future = timer_test();
     block_on(future);
 }
