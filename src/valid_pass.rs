27fca6913e801627bae189bc1287ee1c13c006a4
zolkko <zolkko@gmail.com>
2020-12-15 15:26:14 +0300

    add parsing function

---
+++ src/valid_pass.rs
@@ -0,0 +1,63 @@
+#![feature(str_split_once)]
+use std::error::Error;
+
+
+#[derive(Debug)]
+struct Range {
+    start: usize,
+    end: usize,
+}
+
+#[derive(Debug)]
+struct Input<'a> {
+    range: Range,
+    letter: char,
+    password: &'a str,
+}
+
+
+fn parse_input<'a>(input: &'a str) -> Result<Input<'a>, Box<dyn Error>> {
+
+    let data: Vec<_> = input.split_whitespace().collect();
+    match &data[..] {
+        [range, letter_pat, password] => {
+
+            if let Some((start_str, end_str)) = range.split_once('-') {
+                let start: usize = start_str.parse()?;
+                let end: usize = end_str.parse()?;
+                if start >= end {
+                    return Err("start must be smaller than end part of the range".into());
+                }
+
+                if let Some(letter) = letter_pat.chars().next() {
+                    Ok(Input {
+                        range: Range { start, end },
+                        letter,
+                        password
+                    })
+                } else {
+                    Err("Letter specified in a wrong way".into())
+                }
+            } else {
+                Err("cannot parse range".into())
+            }
+        }
+        _ => Err("invalid input".into()),
+    }
+}
+
+
+fn main() {
+    let inputs = vec![
+        "1-3 a: abcde",
+        "1-3 b: cdefg",
+        "2-9 c: ccccccccc",
+    ];
+
+    for i in inputs {
+        match parse_input(i) {
+            Ok(x) => println!("PARSED: {:?}", x),
+            Err(err) => println!("ERROR: {:?}", err),
+        }
+    }
+}
