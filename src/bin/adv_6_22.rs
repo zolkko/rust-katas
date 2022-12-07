1f709386406aba4cf03dfba72d69bad531540851
zolkko <zolkko@gmail.com>
2022-12-07 09:56:10 +0100

    Add AOC 6-22

---
+++ src/bin/adv_6_22.rs
@@ -0,0 +1,60 @@
+use anyhow::Result as AnyhowResult;
+use itertools::Itertools;
+use std::io;
+use std::io::BufRead;
+
+
+const CHUNK_SIZE: usize = 4;
+
+
+fn all_uniq(v: &[u8]) -> bool {
+    for i in 0..v.len() {
+        for j in (i + 1)..v.len() {
+            if v[i] == v[j] {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+fn find_position(buff: &[u8]) -> Option<usize> {
+    for (i, window) in buff.windows(CHUNK_SIZE).enumerate() {
+        /*
+        if window[0] != window[1] &&
+            window[0] != window[2] &&
+            window[0] != window[3] &&
+            window[1] != window[2] &&
+            window[1] != window[3] &&
+            window[2] != window[3]
+        {
+            return Some(i + CHUNK_SIZE)
+        }
+        */
+
+        if all_uniq(window) {
+            return Some(i + CHUNK_SIZE)
+        }
+    }
+
+    None
+}
+
+fn main() -> AnyhowResult<()> {
+    // let mut guard = io::stdin().lock();
+    // let mut buff = String::new();
+    // guard.read_line(&mut buff)?;
+    // let result = find_position(buff.as_bytes());
+    let result = find_position("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".as_bytes());
+    match result {
+        Some(position) => println!("position is {position}"),
+        None => println!("packed is not found"),
+    }
+    Ok(())
+}
+
+// mjqjpqmgbljsphdztnvjfqwrcgsmlb - 7
+// bvwbjplbgvbhsrlpgdmjqwftvncz: first marker after character 5
+// nppdvjthqldpwncqszvftbrmjlhg: first marker after character 6
+// nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg: first marker after character 10
+// zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw: first marker after character 11
